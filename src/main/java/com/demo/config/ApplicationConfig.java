8b81cbb7576f3a6c6ed96229248216fbc9cc7d5e
sshcherbakov <sshcherbakov@pivotal.io>
2015-11-26 17:11:39 +0100

    Step1 Database config from the next step for better lab preparatino

--- src/main/java/com/demo/config/ApplicationConfig.java
+++ src/main/java/com/demo/config/ApplicationConfig.java
@@ -1,7 +1,12 @@
 package com.demo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import com.demo.controller.DeepThoughtController;
 import com.demo.repository.IDeepThoughtRepository;
@@ -42,5 +47,23 @@ public class ApplicationConfig {
 	public IVogonprotokollService vogonprotokollService() {
 		return new VogonprotokollService();
 	}
+	
+	
+	// TODO Database configuration to separate 
+	
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.H2)
+			.addScript("schema.sql")
+			.addScript("data.sql")
+			.build();
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
 	
 }
