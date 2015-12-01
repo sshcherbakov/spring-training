dd8deb294521d03e9430eadd56cf4043e32c7b01
sshcherbakov <sshcherbakov@pivotal.io>
2015-12-01 09:07:57 +0100

    Fix distinct

--- src/main/java/com/demo/game/repository/GameRepository.java
+++ src/main/java/com/demo/game/repository/GameRepository.java
@@ -8,7 +8,7 @@ import com.demo.game.model.Game;
 //TODO: 2. Require transaction context presence
 public interface GameRepository extends CrudRepository<Game, Long> {
 
-	@Query("select g FROM Game g left join fetch g.positions")
+	@Query("select distinct g FROM Game g left join fetch g.positions")
 	Iterable<Game> findAll();
 	
 }
